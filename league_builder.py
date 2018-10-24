48ba98de23eab8baf69b35792bf7ad81a9318dfc
Pedro de Bragança Cabral <pbragancacabral@gmail.com>
2018-10-24 10:47:14 +0200

    Add Docstrings to public functions

--- league_builder.py
+++ league_builder.py
@@ -11,7 +11,7 @@ The first practice will take place at the Foo Bar High School, Friday at 12:34AM
 players = []
 experienced_players = []
 inexperienced_players = []
-teams = {
+TEAMS = {
     "Sharks": [],
     "Dragons": [],
     "Raptors": []
@@ -19,6 +19,7 @@ teams = {
 
 
 def load_players():
+    """Creates a list of players from a .csv file."""
     with open(CSV_FILE, newline="") as file:
         reader = csv.DictReader(file, delimiter=CSV_DELIMITER)
         rows = list(reader)
@@ -27,6 +28,7 @@ def load_players():
 
 
 def divide_players_by_experience():
+    """Creates two separate lists: players with and without experience."""
     for player in players:
         if player["Soccer Experience"] == "YES":
             experienced_players.append(player)
@@ -35,15 +37,17 @@ def divide_players_by_experience():
 
 
 def assign_players_to_teams():
-    for i in range(len(experienced_players) // len(teams)):
-        for team in teams.values():
+    """Assigns one experienced and one inexperienced player to each team."""
+    for i in range(len(experienced_players) // len(TEAMS)):
+        for team in TEAMS.values():
             team.append(experienced_players.pop())
             team.append(inexperienced_players.pop())
 
 
 def write_teams_file():
+    """Writes a file listing all the team names and their players."""
     with open(TEAMS_FILE, "w") as file:
-        for team_name, team_list in teams.items():
+        for team_name, team_list in TEAMS.items():
             file.write(f"{team_name}\n")
             for player in team_list:
                 file.write(f"{player['Name']}, {player['Soccer Experience']}, {player['Guardian Name(s)']}\n")
@@ -51,7 +55,8 @@ def write_teams_file():
 
 
 def write_guardians_file():
-    for team_name, team_list in teams.items():
+    """Writes a file to each player with an invitation letter."""
+    for team_name, team_list in TEAMS.items():
         for player in team_list:
             first_name, last_name = player["Name"].split()
             with open(f"{first_name}_{last_name}.txt", "w") as file:
