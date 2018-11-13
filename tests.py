eb0f35919ac56fb3134a050719faf768ec861805
Pedro de Bragança Cabral <pbragancacabral@gmail.com>
2018-11-13 18:51:11 +0100

    Add some tests and requirements.txt for coverage tool

--- tests.py
+++ tests.py
@@ -1,8 +1,16 @@
+import os
 import unittest
+
+from league_builder import *
 
 
 class LeagueTests(unittest.TestCase):
-    pass
+    def test_csv_file_exists(self):
+        self.assertTrue(os.path.isfile(CSV_FILE))
+
+    def test_18_players_have_been_loaded(self):
+        load_players()
+        self.assertEqual(len(players), 18)
 
 
 if __name__ == "__main__":
